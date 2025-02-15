f8ac22b9d68524f8a11fe5ee71d138a98ab1e8f0
greenwormstudio <greenwormstudio@gmail.com>
2025-02-15 17:00:48 +0700

    Get all class rooms

---
+++ WCD/wcd_demo_01/src/main/java/org/fai/study/wcd_demo_01/controllers/ClassRoomController.java
@@ -0,0 +1,29 @@
+package org.fai.study.wcd_demo_01.controllers;
+
+import jakarta.persistence.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.fai.study.wcd_demo_01.entities.ClassRoom;
+
+import java.io.IOException;
+@WebServlet("/classroom")
+public class ClassRoomController extends HttpServlet{
+    EntityManagerFactory entityManagerFactory;
+    EntityManager entityManager;
+    @Override
+    public void init() throws ServletException {
+        entityManagerFactory = Persistence.createEntityManagerFactory("default1");
+        entityManager = entityManagerFactory.createEntityManager();
+        System.out.println("Init");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        var classRoom = entityManager.createStoredProcedureQuery("GetAllClass", ClassRoom.class).getResultList();
+        System.out.println("Do get" );
+    }
+}
