3c84f413785bd37cc63354b9f9249bb99ee0d9a9
greenwormstudio <greenwormstudio@gmail.com>
2025-02-18 16:50:42 +0700

    Demo JSP 2

---
+++ WCD/wcd_demo_02/src/main/java/org/fai/study/wcd_demo_02/controllers/ClassRoomController.java
@@ -0,0 +1,34 @@
+package org.fai.study.wcd_demo_02.controllers;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.fai.study.wcd_demo_02.entities.ClassRoom;
+
+import java.io.IOException;
+
+@WebServlet("/classroom")
+public class ClassRoomController extends HttpServlet {
+    EntityManagerFactory entityManagerFactory;
+    EntityManager entityManager;
+    @Override
+    public void init() throws ServletException {
+        entityManagerFactory = Persistence.createEntityManagerFactory("default1");
+        entityManager = entityManagerFactory.createEntityManager();
+        System.out.println("Init");
+    }
+//
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        var classRooms = entityManager.createStoredProcedureQuery("GetAllClass", ClassRoom.class).getResultList();
+        System.out.println("Do get" );
+        req.setAttribute("classRooms", classRooms);
+        req.getRequestDispatcher("/views/classroom/index.jsp").forward(req, resp);
+    }
+}
